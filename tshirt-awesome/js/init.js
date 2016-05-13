14e574e2109b4434e597c608bb89e953e0b9cebb
Leonel Oliveira <leoneloliver@gmail.com>
2016-05-13 10:05:51 -0400

    new project react.js

---
+++ tshirt-awesome/js/init.js
@@ -0,0 +1,181 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+
+});
+
+
+$(".button-collapse").click(function() {
+	window.setTimeout(function(){
+		$("img.cart-tmb").addClass("move");
+	},800);
+});
+
+$(".buy").click(function(){
+  var mid = $(".buy").attr("ng").split(".");
+  var midb = mid[0].split("/");
+  var idx = midb[1]+Math.floor((Math.random() * 10) + 1);
+  
+  $("#base").append('<div class="row row-cart" id="'+idx+'"> <div class="col s6"><img src="'+$(".buy").attr("ng")+'" class="cart-tmb"></div><h6>T-SHIRT</h6> <div class="col s6 white">$ 13,99</div><a href="#" class="delete-cart" id="'+idx+'"><i class="small material-icons">delete</i></a></div>')
+  window.setTimeout(function() {
+		$(".info-cart").addClass("show");
+		window.setTimeout(function(){
+			$(".button-collapse").trigger("click");
+			window.setTimeout(function(){
+				$("img.cart-tmb").addClass("move");
+			},800);
+		}, 1000);
+	}, 1000);
+  removeCart();
+  checkCart();
+});
+
+$("#cart").click(function(){
+  $(".button-collapse").trigger("click");
+});
+
+function checkCart(){
+  var total = $(".row-cart").length;
+  $(".info-cart a.btn-floating.btn-large.waves-effect.waves-light.red").text("");
+  $(".info-cart a.btn-floating.btn-large.waves-effect.waves-light.red").append(total);
+}
+
+
+function removeCart(){
+  $(".row-cart a.delete-cart").each(function(i){
+    $(this).click(function(){
+       //alert($(this).attr("id"));
+       $('#'+$(this).attr("id")).fadeOut().remove();
+       checkCart();
+    });
+  });
+
+}
+
+
+
+// $("#show-tshirt").click(function(){
+//   window.setTimeout(function(){
+//       $(".t-shirt").fadeIn();
+//       $(".store").fadeOut();
+//   },1000);
+  
+// });
+
+// $("#show-store").click(function(){
+//   window.setTimeout(function(){
+//       $(".store").fadeIn();
+//       $(".t-shirt").fadeOut();
+//   },1000);
+  
+// });
+
+// $("#show-all").click(function(){
+//   window.setTimeout(function(){
+//       $(".store").fadeIn();
+//       $(".t-shirt").fadeIn();
+//   },1000);
+  
+// });
+
+$("#close").click(function() {
+  $('.button-collapse').sideNav('hide');
+  $("#show-all").trigger("click");
+});
+
+$(".nw").click(function() {
+  $('.button-collapse').sideNav('hide');
+  //$("#show-all").trigger("click");
+});
+
+window.setTimeout(function(){
+  showImages();
+  addcartBtn();
+  zoomtBtn();
+  
+  $(".link-img").attr('href','#detail');
+  $(".product img").click(function(){
+      // alert();
+      $("#img-large").attr("src",$(this).attr("src"));
+      $(".buy").attr("ng",$(this).attr("src"));
+      $(".info-cart").removeClass("show");
+  });
+  $('a.smoothScroll.link-img').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+  
+  
+},4000);
+
+
+
+function showImages(){
+  $(".col.s12.m6.l3 img").each(function(index) {
+    var times = (index*100);
+    setTimeout(function(){ $(".col.s12.m6.l3 img").eq(index).addClass("showit"); }, times);
+  });
+}
+
+function addcartBtn(){
+    $(".add a").each(function(i){
+      $(this).click(function(){
+        $("#img-large").attr("src",$(this).attr("data-photo"));
+        $(".buy").attr("ng",$(this).attr("data-photo"));
+        $(".info-cart").removeClass("show");
+        $("a.smoothScroll.link-img").eq(i).trigger("click");
+      });
+    });
+}
+
+
+function zoomtBtn(){
+    $(".add a").each(function(i){
+      $(this).mouseover(function(){
+        $(".product img").eq(i).addClass('zoom');
+      });
+      $(this).mouseout(function(){
+        $(".product img").eq(i).removeClass('zoom');
+      });
+    });
+}
+
+
+
+
+
+
+
+
+
---
+++ tshirt-awesome/js/list.js
@@ -0,0 +1,69 @@
+var ProductRow = React.createClass({
+    render: function() {
+        
+       
+        return (
+
+        <div className="col s12 m6 l3">
+          <div className="icon-block">
+         
+            <h5 className="center center-align">{this.props.product.name}</h5>
+            <a className="smoothScroll link-img"><img src={this.props.product.photo} /></a>
+
+            <div className="light price center-align">{this.props.product.price}</div>
+            <div className="add"><a data-photo={this.props.product.photo} className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a></div>
+          </div>
+        </div>
+                
+  
+        );
+    }
+});
+
+var ProductAll = React.createClass({
+    render: function() {
+        var rows = [];
+        // var lastCategory = null;
+        this.props.products.forEach(function(product) {
+            // if (product.category !== lastCategory) {
+            //     rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            // }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            // lastCategory = product.category;
+        });
+        return (
+            <div className="row product">{rows}</div>
+            
+        );
+    }
+});
+
+
+var Mylist = React.createClass({
+    render: function() {
+        return (
+           
+                <ProductAll products={this.props.products} />
+            
+        );
+    }
+});
+
+
+var PRODUCTS = [
+  {price: '$13.99', stocked: true, name: 'HTML', photo: 'img/product1.jpg'},
+  {price: '$13.99', stocked: true, name: 'HTML5' , photo: 'img/product2.jpg'},
+  {price: '$13.99', stocked: true, name: 'CSS' , photo: 'img/product3.jpg'},
+  {price: '$13.99', stocked: true, name: 'JAVASCRIPT' , photo: 'img/product4.jpg'},
+  {price: '$13.99', stocked: true, name: 'JQUERY' , photo: 'img/product5.jpg'},
+  {price: '$13.99', stocked: true, name: 'ANGULAR' , photo: 'img/product6.jpg'},
+  {price: '$13.99', stocked: true, name: 'FRONT END' , photo: 'img/product7.jpg'},
+  {price: '$13.99', stocked: true, name: 'REACTJS' , photo: 'img/product8.jpg'},
+];
+ 
+ReactDOM.render(
+    <Mylist products={PRODUCTS} />,
+    document.getElementById('prod')
+);
+
+
